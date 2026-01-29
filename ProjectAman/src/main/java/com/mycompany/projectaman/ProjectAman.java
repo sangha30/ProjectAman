7fb7ee5b977b20a550c8e969e249465e34729349
sangh <sangh@AMANPC>
2026-01-28 21:59:20 -0500

    Added my name

---
+++ ProjectAman/src/main/java/com/mycompany/projectaman/ProjectAman.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectaman;
+
+/**
+ *
+ * @author sangh
+ */
+public class ProjectAman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Aman.");
+    }
+}
