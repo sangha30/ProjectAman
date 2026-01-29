70371ea05da3a16f83d6258d2bffb3d26c7af468
aman <sangha30@sheridancollege.ca>
2026-01-28 22:52:17 -0500

    This is task 4.

---
+++ ProjectAman/src/main/java/com/mycompany/projectaman/ProjectAman.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectaman;
+
+/**
+ *
+ * @author sangh
+ */
+public class ProjectAman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is task 4!");
+
+    }
+}
